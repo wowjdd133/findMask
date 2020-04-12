93205a9d1c833e0e1b8530b9ad41c781a51bb74d
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-04-12 17:10:20 +0900

    [Change] eslint.js
    eslint.js 설정 변경

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  "extends": "standard",
+  "rules": {
+      "indent": [
+          "error",
+          4
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "no-trailing-spaces": 0,
+      "keyword-spacing": 0,
+      "no-unused-vars": 1,
+      "no-multiple-empty-lines": 0,
+      "space-before-function-paren": 0,
+      "eol-last": 0
+  }
 };
