10519e2949a143e8d3accc977359a1164eb9e11d
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-04-19 17:17:15 +0900

    [ADD] eslintrc.js
    eslintrc.js 추가, eslint 추가로 인한 yarn lock 변경

---
+++ front/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["airbnb", "airbnb/hooks"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: ["js", "jsx"] }],
+    "linebreak-style": 0,
+    quotes: ["off", "single"],
+  },
+};
