1d61e069d69b691af7bb8fe843160b8c43d7120b
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-04-26 14:03:44 +0900

    [ADD] front Pages
    Main, map, world

---
+++ front/src/pages/Map.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MapContainer from "../components/map";
+
+const Map = () => <MapContainer />;
+
+export default Map;
---
+++ front/src/pages/World.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const World = () => (
+  <div>
+    <h1>World</h1>
+    <h1>World</h1>
+    <h1>World</h1>
+    <h1>World</h1>
+  </div>
+);
+
+export default World;
