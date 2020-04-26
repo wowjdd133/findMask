0bcc44375b296cd1b05cec7020b7a56dfc79400b
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-04-26 19:16:34 +0900

    [ADD] component.header
    헤더 추가

---
+++ front/src/components/header/header.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+import React from "react";
+
+import styled from "styled-components";
+
+import { Link } from "react-router-dom";
+
+const Menu = styled.div`
+  position: fixed;
+  width: 240px;
+  left: 0;
+  height: 100%;
+  z-index: 1;
+  border-right: 1px solid #bdbdbd;
+`;
+
+const Menu_logo = styled.div`
+  background-image: url("images/logo.jpg");
+  width: 240px;
+  height: 120px;
+  background-size: cover;
+  border-bottom: 1px solid #000000;
+`;
+
+const Menu_ul = styled.ul`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Menu_ll = styled.li`
+  text-align: center;
+  width: 100%;
+  height: 50px;
+  margin-top: 25px;
+  font-size: 18px;
+`;
+
+const header = () => (
+  <Menu>
+    <Menu_logo />
+    <Menu_ul>
+      <Menu_ll>
+        <Link to="/map">공적 마스크 확인하기</Link>
+      </Menu_ll>
+      <Menu_ll>
+        <Link to="/world">세계 코로나 정보 확인하기</Link>
+      </Menu_ll>
+      <Menu_ll>
+        <Link to="/">메인으로 돌아가기</Link>
+      </Menu_ll>
+    </Menu_ul>
+  </Menu>
+);
+
+export default header;
