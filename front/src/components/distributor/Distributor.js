f207eed1eeb940e6ebd0ffefb33bf82c9a94f273
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-05-25 12:24:47 +0900

    [Update] Distributor
    add state, handleChange, css, 데이터 정렬 등 추가. loading도 추가.

--- front/src/components/distributor/Distributor.js
+++ front/src/components/distributor/Distributor.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { observer, inject } from "mobx-react";
 import DistributorList from "./DistributorList";
@@ -9,7 +9,38 @@ const DistributorContainer = styled.div`
   height: 100%;
 `;
 
+const DistributorSearchBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: 10%;
+  background-color: #0000ff;
+  box-shadow: 3px 3px gray;
+  margin-bottom: 3px;
+`;
+
+const DistributorSearch = styled.input`
+  width: 20%;
+  height: 35%;
+  background: #fff;
+`;
+
 const Distributor = ({ store }) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchList = () => {
+    const data = store.MapStore.maskData.stores.filter((item) => {
+      return item.name.indexOf(search) > -1;
+    });
+
+    return <DistributorList data={data} />;
+  };
+
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -21,8 +52,10 @@ const Distributor = ({ store }) => {
             position.coords.latitude,
             position.coords.longitude
           ).then(() => {
-            store.MapStore.sortData().then(() => {
-              store.MapStore.loadingComplete();
+            store.MapStore.changeTimeToElapsedTime().then(() => {
+              store.MapStore.sortData().then(() => {
+                store.MapStore.loadingComplete();
+              });
             });
           });
         });
@@ -31,9 +64,15 @@ const Distributor = ({ store }) => {
   }, []);
   return (
     <DistributorContainer>
-      {!store.MapStore.loading && (
-        <DistributorList data={store.MapStore.maskData.stores} />
-      )}
+      <DistributorSearchBox>
+        <DistributorSearch onChange={handleChange} value={search} />
+      </DistributorSearchBox>
+      {!store.MapStore.loading &&
+        (search ? (
+          searchList()
+        ) : (
+          <DistributorList data={store.MapStore.maskData.stores} />
+        ))}
     </DistributorContainer>
   );
 };
