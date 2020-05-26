8a1563b8944fce83ee1024c0b83e34a9bec5865d
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-05-27 08:52:34 +0900

    [ADD] DataTextContent
    main.js의 DataTextContent 컴포넌트 분리

---
+++ front/src/components/corona/main/DataTextContent.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const DataTextContentStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 2rem;
+  font-weight: 700;
+  width: ${(props) => (props.size === "big" ? "80%" : "30%")};
+  opacity: 0.7;
+  background: ${(props) => props.color};
+  border: 2px solid #ccc;
+  border-radius: 15px;
+  height: 100%;
+  margin: 0px 15px 0px 15px;
+`;
+
+const DataTextContent = (props) => {
+  return (
+    <DataTextContentStyle size={props.size}>
+      {props.children}
+    </DataTextContentStyle>
+  );
+};
+
+export default DataTextContent;
