74eb938548b5c5e721d878753711ff66739ce3a3
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-05-27 19:45:06 +0900

    [Update] index.js
    store 추가

--- front/src/stores/index.js
+++ front/src/stores/index.js
@@ -1,9 +1,11 @@
 import MapStore from "./mapStore";
 import CoronaStore from "./coronaStore";
+import CoronaWorldStore from "./coronaWorldStore";
 
 const stores = {
   MapStore: new MapStore(),
   CoronaStore: new CoronaStore(),
+  CoronaWorldStore: new CoronaWorldStore(),
 };
 
 export default stores;
