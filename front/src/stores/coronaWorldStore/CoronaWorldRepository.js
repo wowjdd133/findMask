f0ed930b47637a8664d074ac4f2b1e9051ffcdd8
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-05-29 12:12:20 +0900

    [Update] CoronaWolrdStore, Repository
    Store: sleep 추가, worldWIPData 추가 받아오고 시간 받아오고 추가.
    Repository: worldWIpDAtaToISODate 추가.

--- front/src/stores/coronaWorldStore/CoronaWorldRepository.js
+++ front/src/stores/coronaWorldStore/CoronaWorldRepository.js
@@ -9,6 +9,19 @@ class CoronaWorldRepository {
       console.error(err);
     }
   }
+
+  async getWorldWIPDataToISODate(prevTime, nowTime) {
+    console.log(`/world?from=${prevTime}&to=${nowTime}`);
+    try {
+      const { data } = await coronaAPI.get(
+        `/world?from=${prevTime}&to=${nowTime}`
+      );
+      //api.covid19api.com/world?from=2020-05-21T00:00:00Z&to=2020-05-28T00:00:00Z
+      https: return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 export default new CoronaWorldRepository();
--- front/src/stores/coronaWorldStore/CoronaWorldStore.js
+++ front/src/stores/coronaWorldStore/CoronaWorldStore.js
@@ -1,18 +1,61 @@
-import { action, observable } from "mobx";
+import { action, observable, computed } from "mobx";
 import CoronaWorldRepository from "./CoronaWorldRepository";
+import moment from "moment";
 
 class CoronaWorldStore {
   @observable.ref
   worldData = [];
+  @observable.ref
+  worldWIPData = [];
 
   @action
   async getWorldData() {
     try {
+      this.sleep(2000);
+      console.log("get worldData");
       const data = await CoronaWorldRepository.getWorldData();
       this.worldData = data;
     } catch (err) {
       console.error(err);
     }
+  }
+
+  sleep(delay) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, delay);
+    });
+  }
+
+  @action
+  async getWorldWIPDataToISODate() {
+    try {
+      console.log("get worldWPIData");
+      const now = this.getNowTimeToTZero();
+      const prev = this.getTimeToPrev();
+      const data = await CoronaWorldRepository.getWorldWIPDataToISODate(
+        prev.toISOString(),
+        now.toISOString()
+      );
+      this.worldWIPData = data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  getNowTimeToTZero() {
+    let now = moment();
+    now.set("hour", 0);
+    now.set("minute", 0);
+    now.set("second", 0);
+    now.set("millisecond", 0);
+
+    return now;
+  }
+
+  getTimeToPrev() {
+    let prev = moment();
+    prev.subtract(90, "days");
+    return prev;
   }
 }
 
