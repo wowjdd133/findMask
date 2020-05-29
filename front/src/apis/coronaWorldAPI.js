c4527d311fdb9fcba9de4797a3b29d333bc0a205
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-05-29 12:09:53 +0900

    [Update] coronaWorldAPI
    전체 get에 sleep을 거는 것은 get이 2~3개가 넘을 경우 6초 이상의 지연시간이 걸려서 변경.

--- front/src/apis/coronaWorldAPI.js
+++ front/src/apis/coronaWorldAPI.js
@@ -4,21 +4,4 @@ const coronaWorldAPI = axios.create({
   baseURL: "https://api.covid19api.com/",
 });
 
-const sleep = (delay) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  });
-};
-
-coronaWorldAPI.interceptors.request.use(
-  async (response) => {
-    await sleep(2000);
-    return response;
-  },
-  (err) => {
-    console.error(err);
-    return Promise.reject(err);
-  }
-);
-
 export default coronaWorldAPI;
